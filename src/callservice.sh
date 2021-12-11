c0aa7914e7530440469c731b3496d7e87af66956
xpandi <melodymodi23@gmail.com>
2021-12-11 01:40:38 -0800

    callservice update

--- src/callservice.sh
+++ src/callservice.sh
@@ -5,7 +5,7 @@ json={"\"bucketname\"":"\"test.bucket.sales.dimo\"","\"filename\"":"\"$1\""}
 
 echo "Invoking Lambda function using AWS CLI"
 #time output=`aws lambda invoke --invocation-type RequestResponse --function-name {LAMBDA-FUNCTION-NAME} --region us-east-2 --payload $json /dev/stdout | head -n 1 | head -c -2 ; echo`
-{ time output=`aws lambda invoke --invocation-type RequestResponse --function-name service1 --region us-east-2 --payload $json /dev/stdout` ; } 2> "$1.output"
+{ time output=`aws lambda invoke --invocation-type RequestResponse --function-name t6rds --region us-east-2 --payload $json /dev/stdout` ; } 2> "$1.output"
 
 #echo ""
 #echo "JSON RESULT:"
